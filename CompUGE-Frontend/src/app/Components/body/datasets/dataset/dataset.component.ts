343fbe99bac08c33bfc1c5cd54676bff7a7b5936
Ahmad Shallouf <ahmadshallof.as@gmail.com>
2024-08-04 17:35:13 +0200

    finalized the design of the datasets tab

--- CompUGE-Frontend/src/app/Components/body/datasets/dataset/dataset.component.ts
+++ CompUGE-Frontend/src/app/Components/body/datasets/dataset/dataset.component.ts
@@ -1,12 +1,18 @@
 import {Component, Input} from '@angular/core';
 import {MatCard, MatCardContent} from "@angular/material/card";
+import {MatButton} from "@angular/material/button";
+import {MatExpansionPanel, MatExpansionPanelHeader, MatExpansionPanelTitle} from "@angular/material/expansion";
 
 @Component({
   selector: 'app-dataset',
   standalone: true,
   imports: [
     MatCard,
-    MatCardContent
+    MatCardContent,
+    MatButton,
+    MatExpansionPanel,
+    MatExpansionPanelHeader,
+    MatExpansionPanelTitle
   ],
   templateUrl: './dataset.component.html',
   styleUrl: './dataset.component.css'
@@ -34,4 +40,17 @@ export class DatasetComponent {
     return window.URL.createObjectURL(new Blob(data, {type: 'text/plain'}));
   }
 
+  downloadData(data: string[], name: string) {
+    const url = this.getTextDownloadURL(data);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = this.task + '-' + this.name + '-' + name + '.csv';
+    document.body.appendChild(a);
+    a.click();
+    window.URL.revokeObjectURL(url);
+  }
+
+  goToUrl(url: string) {
+    window.open(url, '_blank');
+  }
 }
