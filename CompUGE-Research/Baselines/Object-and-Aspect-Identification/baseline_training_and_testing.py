e4fbee632afed4795f47f6599ffe6561b35eef7a
Ahmad Shallouf <ahmadshallof.as@gmail.com>
2024-09-09 22:56:10 +0200

    test on merged

--- CompUGE-Research/Baselines/Object-and-Aspect-Identification/baseline_training_and_testing.py
+++ CompUGE-Research/Baselines/Object-and-Aspect-Identification/baseline_training_and_testing.py
@@ -209,9 +209,7 @@ def main():
             if dataset_info["task"] != "Object and Aspect Identification":
                 continue
             train_folder = f"../../Splits/{dataset_info['folder']}"
-            test_folders = [f"../../Splits/{other_dataset['folder']}" for other_dataset in datasets_metadata["datasets"] if
-                            other_dataset["task"] == "Object and Aspect Identification"
-                            and "merged" not in other_dataset["folder"]]
+            test_folders = [f"../../Splits/oai_webis_beloucif_merged"]
             train_and_test_on_datasets(train_folder, test_folders, results_folder, model_name)
 
 if __name__ == "__main__":
